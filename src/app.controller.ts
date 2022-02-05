4a401a23a84bc92598bf084a2e415a6d94337da3
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-02-05 13:34:31 -0700

    modified app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello')
+  @Post('login')
+  login(): any {
+    return {}
+  }
+
+  @Get('protected')
   getHello(): string {
     return this.appService.getHello();
   }
+  // @Get('hello')
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 }
