79a7919f5f30b32ff2ce6316761d31a30feeb213
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-02-05 13:29:24 -0700

    removed providers

--- src/providers/providers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProvidersService } from './providers.service';
-import { ProvidersController } from './providers.controller';
-
-@Module({
-  controllers: [ProvidersController],
-  providers: [ProvidersService]
-})
-export class ProvidersModule {}
