507d546be0a82b8b7f12e9ca1e1b54359b794f8e
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-02-05 12:22:42 -0700

    add validation pipes and providers module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // pipes to be used throughout entire applicatoin
+  app.useGlobalPipes(new ValidationPipe());
 
   // configuration for swagger documentation
   const config = new DocumentBuilder()
