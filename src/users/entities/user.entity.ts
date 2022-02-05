1b38632dae3edc60574c9776d8d6bd7669517cb0
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-02-05 11:40:58 -0700

    swagger documentation set up

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,12 @@
 // class the represents the shape of the user object
+
+import { ApiProperty } from "@nestjs/swagger";
+
 // if was connecting to a databse this entity would map one to one to the user DB table
 export class User {
+    @ApiProperty()
     id: number;
+
+    @ApiProperty()
     name: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,25 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+// tag categorizes api calls in the swagger docs
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
+    // clarify the OK response from the API
+    @ApiOkResponse({type: User, isArray: true})
     // get all users
     @Get()
     getUsers(): User[] {
         return this.usersService.findAll();
     }
 
+    // clarify the OK response from the API
+    @ApiOkResponse({type: User})
     // get specific user based off id
     @Get(':id')
     getUserById(@Param('id') id: string): User { // TODO: autoparse id
@@ -22,6 +27,8 @@ export class UsersController {
     }
 
     // EXTENDED FUNCTIONALITY
+    // tag indicates the response type for the swagger docs
+    @ApiCreatedResponse({type: User})
     // post call for creating a user
     @Post()
     createUser(@Body() body: CreateUserDto): User {
