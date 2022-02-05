1e6debc1a82db5f2778bf94ffe16bd7fa65f409b
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-02-05 12:04:55 -0700

    added basic error handling example

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, NotFoundException, Param, Post, Query } from '@nestjs/common';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -12,18 +12,28 @@ export class UsersController {
 
     // clarify the OK response from the API
     @ApiOkResponse({type: User, isArray: true})
+    // indicate optional parameters
+    @ApiQuery({name: 'name', required: false})
     // get all users
     @Get()
-    getUsers(): User[] {
-        return this.usersService.findAll();
+    getUsers(@Query('name') name: string): User[] {
+        return this.usersService.findAll(name);
     }
 
     // clarify the OK response from the API
     @ApiOkResponse({type: User})
+    @ApiNotFoundResponse()
     // get specific user based off id
     @Get(':id')
     getUserById(@Param('id') id: string): User { // TODO: autoparse id
-        return this.usersService.findById(Number(id))
+
+        const user = this.usersService.findById(Number(id));
+
+        if (!user) {
+            throw new NotFoundException();
+        }
+
+        return user;
     }
 
     // EXTENDED FUNCTIONALITY
