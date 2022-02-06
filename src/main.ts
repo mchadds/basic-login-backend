964184617e7b417aa59649e5f26c83f5386cac96
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-02-05 17:28:55 -0700

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // pipes to be used throughout entire applicatoin
   app.useGlobalPipes(new ValidationPipe());
